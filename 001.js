2f7b636418f6428bfeab473a43bbc53aed233da9
João pedro <joaopedroribeiromonteiro@gmail.com>
2025-09-01 21:24:52 -0300

    primeiro commit

---
+++ 001.js
@@ -0,0 +1,32 @@
+//PRIMEIRA ATIVIDADE--01
+
+let alunos = [];
+let números= [1, 2, 3, 4, 5];
+
+alunos.push("Alice"); 
+alunos.unshift("Bob");
+alunos.push("Charlie");
+alunos.splice(1, 0, "David");
+alunos.pop();
+alunos.shift();
+
+//chatgpt me orientou
+let middleIndex = Math.console.log(items[0].name);
+floor(alunos.length / 2);
+alunos.splice(middleIndex, 1);
+
+alunos[1] = "Eve";
+let aliceIndex =alunos.indexOf("Alice");
+let hasBob = alunos.includes("Bob");
+let noZoe = !alunos.includes("Zoe");
+
+let contagemdealunos = alunos.length;
+let arr = [10, 20, 30, 40];
+let arrLength = arr.length;
+
+console.log("alunos:", alunos);
+console.log("aliceIndex:", aliceIndex);
+console.log("hasBob:", hasBob);
+console.log("noZoe:", noZoe);
+console.log("contagemdealunos:", contagemdealunos);
+console.log("arrLength:", arrLength);
